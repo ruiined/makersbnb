fd0928477f56ecdf2a286ec27e340992b0ecbc9f
ruiined <ruiined@gmail.com>
2022-03-22 14:01:30 +0000

    db migrations

---
+++ db_migrations/04_create_properties_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE properties (id SERIAL PRIMARY KEY, title VARCHAR(80), description VARCHER(500), address VARCHAR(80), price FLOAT(8), image_url VARCHAR(80));
---
+++ db_migrations/05_create_bookings_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE bookings (id SERIAL PRIMARY KEY, property_id INT(4), user_id INT(4));
